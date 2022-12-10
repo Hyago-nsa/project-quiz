c611e49e7fb3f3139a7b4ecff09206d0a9669795
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-12-10 02:55:42 -0300

    Updates

---
+++ precache-manifest.e7074f48bc9970fcbc8d9a1eb929e64a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c2c3eb5bc66929ef78b99159992673",
+    "url": "/project-quiz/index.html"
+  },
+  {
+    "revision": "d7c29819d63660b90606",
+    "url": "/project-quiz/static/css/main.968643b6.chunk.css"
+  },
+  {
+    "revision": "19a02f0edfd296cac57c",
+    "url": "/project-quiz/static/js/2.0c67da29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-quiz/static/js/2.0c67da29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c29819d63660b90606",
+    "url": "/project-quiz/static/js/main.e417b569.chunk.js"
+  },
+  {
+    "revision": "59ed93652b2687d918d9",
+    "url": "/project-quiz/static/js/runtime-main.02df9e7e.js"
+  }
+]);
